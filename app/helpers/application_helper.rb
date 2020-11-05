b229a7d4a90e75a53553c2dd062a2701eef4da18
divyayuva296 <divyayuvasoft296@gmail.com>
2020-11-05 11:23:54 +0530

    indentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
   		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
   		link_to title, {:sort => column, :direction => direction}, {:class => css_class}
 	end
+
+	def show_errors(object, field_name)
+  		if object.errors.any?
+    		if !object.errors.messages[field_name].blank?
+      			object.errors.messages[field_name].join(", ")
+    		end
+  		end
+	end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :email, presence: true
+  validates :email, presence: true,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },uniqueness: true
+  validates :password, length: {minimum: 6},presence: true
+  validates :password, confirmation: {case_sensitive: true}
   # validates :password, presence: true
 end
